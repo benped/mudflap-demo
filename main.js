ac8187001171950f4b7ba22f9e3c71a4313469a2
Ben Pedrick <bpedrick@gmail.com>
2022-10-03 21:48:30 -0500

    logos working properly

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import asphalt from "./textures/asphalt.jpeg";
+import { Object3D } from "three";
 
 const scene = new THREE.Scene();
 // scene.background = new THREE.Color("light-gray");
@@ -45,9 +46,9 @@ const ambient = new THREE.AmbientLight(0x404040, 3);
 // const helper = new THREE.PointLightHelper(directional, 5);
 
 const directional = new THREE.DirectionalLight(0xefebd8, 1);
-const helper = new THREE.DirectionalLightHelper(directional, 10);
+// const helper = new THREE.DirectionalLightHelper(directional, 10);
 
-scene.add(helper);
+// scene.add(helper);
 
 directional.position.set(1, 20, 0);
 
@@ -57,33 +58,41 @@ scene.add(directional);
 cube.position.set(0, 0, 0);
 scene.add(cube);
 
-
+let logoObject = new THREE.Object3D()
+scene.add(logoObject)
 
 let planeGeometry = new THREE.PlaneGeometry(12, 9, 1, 1);
 planeGeometry.rotateY(1.5708);
 planeGeometry.translate(.131,-5,0)
 
-let activeLogo = false;
+
 
 let minimizerButton = document.getElementById("min-logo");
 minimizerButton.addEventListener("click", () => {
-  if (activeLogo){
-    scene.clear(logoPlane)
-  }
-  activeLogo = true;
+  logoObject.clear()
   let minimizerLogo = new THREE.TextureLoader().load('2019_MinimizerRegisteredOutlinedLogo.png')
   // let img = new THREE.TextureLoader().load( minimizerLogo );
   let planeMaterial = new THREE.MeshBasicMaterial( { map: minimizerLogo, transparent: true } );
   let logoPlane = new THREE.Mesh(planeGeometry, planeMaterial);
-  scene.add(logoPlane)
+  logoObject.add(logoPlane)
+})
+
+let highBarButton = document.getElementById("hbb-logo");
+highBarButton.addEventListener("click", () => {
+  logoObject.clear()
+  let HBBLogo = new THREE.TextureLoader().load('HBB_Logo_blk.png')
+  // let img = new THREE.TextureLoader().load( minimizerLogo );
+  let planeMaterial = new THREE.MeshBasicMaterial( { map: HBBLogo, transparent: true } );
+  let logoPlane = new THREE.Mesh(planeGeometry, planeMaterial);
+  logoObject.add(logoPlane)
 })
 
 let clearButton = document.getElementById("no-logo");
 clearButton.addEventListener("click", () => {
-  if (activeLogo){
 
-    scene.clear(logoPlane)
-  }
+    console.log("clear press and logo 'active'")
+    logoObject.clear()
+
 })
 
 
